2ae5fb166e69a91caf0bcb25fe09a36f464b9233
Sliim <sliim@mailoo.org>
2012-11-04 00:32:44 +0100

    Add custom.el file

---
+++ custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-trailing-lines nil)
+ '(ecb-history-make-buckets (quote never))
+ '(ecb-kill-buffer-clears-history (quote auto))
+ '(ecb-layout-name "sliim-left-layout")
+ '(ecb-layout-window-sizes (quote (("sliim-left-layout" (ecb-directories-buffer-name 0.20725388601036268 . 0.7454545454545455) (ecb-history-buffer-name 0.20725388601036268 . 0.23636363636363636)) ("sliim-layout" (ecb-directories-buffer-name 0.19047619047619047 . 0.9811320754716981) (ecb-methods-buffer-name 0.21164021164021163 . 0.6226415094339622)))))
+ '(ecb-options-version "2.40")
+ '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
+ '(ecb-show-sources-in-directories-buffer (quote ("left7" "left13" "left14" "left15" "sliim-layout" "sliim-left-layout")))
+ '(etags-select-extended-matches t)
+ '(global-auto-revert-mode t)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (2 ((shift) . 2) ((control)))))
+ '(php-project-list (quote (("sleemacs" "~/projects/sleemacs/" "~/projects/sleemacs/.emacs/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "" "") ("Wdb" "~/projects/Wdb/" "~/projects/Wdb/.emacs/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "Wdb" "Wdb") ("DbPatcher" "~/projects/DbPatcher/" "~/projects/DbPatcher/.emacs/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "DbPatcher" "") ("Zend" "~/opt/zf2/library/Zend/" "~/opt/zf2/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "Zend" "Zend") ("SLiib" "~/projects/SLiib/" "~/projects/SLiib/.emacs/TAGS" nil "~/projects/SLiib/tests/phpunit.xml" nil (("" . "") "" "" "" "" "" "" "" "") "SLiib" "SLiib"))))
+ '(phpcs-shell-command "/usr/bin/phpcs")
+ '(phpcs-standard "Sliim")
+ '(phpunit-shell-command "/usr/bin/phpunit")
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 10000)
+ '(scroll-step 1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-candidate-face ((t (:background "gray50" :foreground "black"))))
+ '(ac-candidate-mouse-face ((t (:background "gray50" :foreground "black"))))
+ '(ac-etags-candidate-face ((t (:background "grey50" :foreground "black"))) t)
+ '(ac-etags-selection-face ((t (:background "grey15" :foreground "steelBlue"))) t)
+ '(ac-selection-face ((t (:background "grey15" :foreground "steelBlue"))))
+ '(ecb-default-highlight-face ((((class color) (background dark)) (:background "grey15")))))

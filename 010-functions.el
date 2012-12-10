571b32329ad07429f543207b3045a977b54b3997
Sliim <sliim@mailoo.org>
2012-12-10 22:59:42 +0000

    Add function that print json string (on current region) into human readable format.

--- 010-functions.el
+++ 010-functions.el
@@ -29,16 +29,9 @@
 
 ;;; Code:
 
-(defun ecb-refresh ()
-  "Refresh ecb window (directories, methods, history)"
-  (interactive)
-  (ecb-update-directories-buffer)
-  (ecb-clear-history)
-  (ecb-rebuild-methods-buffer))
-
 (defun my-php-mode-hook ()
-  (interactive)
   "Function to be called when entering into php-mode."
+  (interactive)
   (when (and (require 'auto-complete nil t) (require 'auto-complete-config nil t))
     (make-local-variable 'ac-sources)
     (setq ac-sources '(ac-source-words-in-same-mode-buffers
@@ -46,3 +39,16 @@
     (when (require 'auto-complete-etags nil t)
       (add-to-list 'ac-sources 'ac-source-etags))
     (auto-complete-mode t)))
+
+(defun ecb-refresh ()
+  "Refresh ecb window (directories, methods, history)"
+  (interactive)
+  (ecb-update-directories-buffer)
+  (ecb-clear-history)
+  (ecb-rebuild-methods-buffer))
+
+(defun json-to-human-format ()
+  "Print json string into an human readable format.
+This function run external shell command `python -m json.tool` on current region."
+  (interactive)
+  (shell-command-on-region (region-beginning) (region-end) "python -m json.tool"))

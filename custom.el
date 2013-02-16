c607cb35c82f8363005591bcc58ac6fea2c6e7c7
Sliim <sliim@mailoo.org>
2013-02-16 21:12:24 +0100

    Add helm custom (default face)

--- custom.el
+++ custom.el
@@ -96,3 +96,11 @@
  '(cursor ((t (:background "LightSkyBlue")))))
 
 ;;; custom.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-grep-match ((t (:background "color-32" :foreground "black"))))
+ '(helm-match ((t (:background "color-32" :foreground "black"))))
+ '(helm-selection ((t (:background "#628c98" :foreground "black" :underline nil)))))

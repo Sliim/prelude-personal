e9d3af4d294391499944bc94b9fcadb5fdac4422
Sliim <sliim@mailoo.org>
2013-08-20 19:09:00 +0200

    Fixed eshell prompt.
    Fixed eshell prompt when attempt to get unpushed
    commits on local branch that has not been upstreamed.

--- 010-functions.el
+++ 010-functions.el
@@ -83,7 +83,7 @@ This function run external shell command `python -m json.tool` on current region
   "Return number of unpushed commits in repository `PWD`."
   (when (and (eshell-search-path "git")
              (locate-dominating-file pwd ".git"))
-    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git log @{u}.. --oneline | wc -l"))))
+    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git log @{u}.. --oneline 2>/dev/null | wc -l"))))
       (let ((out (substring git-output 0 -1)))
         (when (not (string= out "0"))
           (propertize (concat " [" out "]")

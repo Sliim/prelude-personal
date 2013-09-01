261678cba10294478ee689be021d28165d5e8fac
Sliim <sliim@mailoo.org>
2013-09-01 23:31:12 +0200

    Remap \e[1;5* to C-<arrow_key>

--- 020-config.el
+++ 020-config.el
@@ -104,7 +104,11 @@
       (define-key input-decode-map "\e[1;2A" [S-up])
       (define-key input-decode-map "\e[1;2B" [S-down])
       (define-key input-decode-map "\e[1;2C" [S-right])
-      (define-key input-decode-map "\e[1;2D" [S-left])))
+      (define-key input-decode-map "\e[1;2D" [S-left])
+      (define-key input-decode-map "\e[1;5A" [C-up])
+      (define-key input-decode-map "\e[1;5B" [C-down])
+      (define-key input-decode-map "\e[1;5C" [C-right])
+      (define-key input-decode-map "\e[1;5D" [C-left])))
 
 ;; Etags-select
 (require 'etags-select)

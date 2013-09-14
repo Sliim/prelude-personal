a8ba022e9ad4bfdceb1700b9eb5385b96d64ffdc
Sliim <sliim@mailoo.org>
2013-09-14 14:37:07 +0200

    Disabled mouse integration

--- 020-config.el
+++ 020-config.el
@@ -42,9 +42,17 @@
       scroll-step 1
       delete-trailing-lines t
       global-auto-revert-mode t
-      mouse-wheel-progressive-speed nil
-      mouse-wheel-scroll-amount (quote (2 ((shift) . 2) ((control))))
       vc-follow-symlinks t)
+
+;; Mouse settings (disabling mouse-wheel and mouse keys)
+(setq make-pointer-invisible t
+      mouse-wheel-mode -1)
+(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+  (global-unset-key k))
 
 ;; Print config
 (setq ps-font-size 8
